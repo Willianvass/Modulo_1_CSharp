335f404189e60cbbae315511208fca933114e437
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-04-12 22:58:07 -0300

    Add files via upload

---
+++ Aula 10 - Trabalhando Datas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Date_Time
+{
+    public class MyClass
+    {
+        public static void Main(string[] args)
+        {  
+            var now = DateTime.Now;
+            //Listagem dos formatos que exitem para datas
+            System.Console.WriteLine("Formato: ");
+            var formats = new string[] {"d","D","f","F","g","G","m","o","r","s","t","T","u","U","Y"};
+
+            foreach (var format in formats)
+            {
+                //2 formas de imprimir o resultado
+                System.Console.WriteLine("Data no Formato {0}: {1}", format, now.ToString(format));
+                //Usando o $ se chama interpolação
+                //System.Console.WriteLine($"Data no formato {format} : {now.ToString(format)}");
+            }
+            //Maneiras de formatar manualmente
+            System.Console.WriteLine($"Data no format d: {now:d}");
+            System.Console.WriteLine($"Data no formato MM-dd-yyyy: {now:MM-dd-yyyy}");
+            
+            var dateFormat = now.ToString("MM-dd-yyyy");
+            var dateFormatBr = now.ToString("dd/MM/yyyy HH:mm:ss");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula 11 - Arquivos e Diretorios/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Arquivos_Diretorios
+{
+    public class MyClass
+    {
+        static void Main(string[] args)
+        {
+            var folderName = "pasta";
+            var subFolderName = "subpasta";
+            var subFolderNameStatic = "subpasta_usingstatic";//Para diferenciar da Directory e DirectoryInfo
+
+            //Criando objeto instanciando a subpasta
+            var directoryInfoSubFolder = new DirectoryInfo(subFolderName);
+            //Verificando se existe a pasta
+            if (!Directory.Exists(subFolderName) || directoryInfoSubFolder.Exists)
+            {
+                //Criando a pasta 2 maneiras, create se tiver instanciado > Estão na pasta BIN quando criadas
+                Directory.CreateDirectory(subFolderNameStatic);
+                Directory.CreateDirectory(folderName);
+                directoryInfoSubFolder.Create();
+                //Movendo de uma para outra pasta > Colocando a Subpasta para dentro da pasta
+                directoryInfoSubFolder.MoveTo($"{folderName}//{subFolderName}");
+                //Outra maneira para mostrar o caminho das pastas usando o @ e \\ 
+                Directory.Move(subFolderNameStatic, @$"{folderName}\\{subFolderNameStatic}");
+            }
+
+            var name = directoryInfoSubFolder.Name;
+            var parent = directoryInfoSubFolder.Parent;
+            var root = directoryInfoSubFolder.Root;
+            var exist = directoryInfoSubFolder.Exists;
+
+            //Como percorrer dentro das pastas
+            foreach (var directory in Directory.GetDirectories(folderName))
+            {
+                System.Console.WriteLine(directory);
+            }
+            //Deletando a subFolderName
+            Directory.Delete($@"{folderName}\\{subFolderName}");
+
+            //Criando a variavel e passando o caminho
+            var file = @"pasta\texto.txt";
+            //Verificando se o arquivo existe
+            if (!File.Exists(file))
+            {
+                File.CreateText(file);
+            }
+            //Incializando o FileInfo passando o caminho
+            var fileInfo = new FileInfo(file);
+
+            System.Console.WriteLine($"Nome: {fileInfo.Name}, Tamanho: {fileInfo.Length}, Data de Atualização: {fileInfo.LastWriteTime}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
